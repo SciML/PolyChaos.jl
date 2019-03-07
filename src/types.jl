1096fe4e870031277a5c075de94ab7a333113d8c
timueh <tillmann.muehlpfordt@kit.edu>
2019-03-07 21:42:15 +0100

    code hygiene -- types.jl

--- src/types.jl
+++ src/types.jl
@@ -13,141 +13,76 @@ struct Measure
     symmetric::Bool
     pars::Dict
     function Measure(name::String,w::Function,dom::Tuple{Real,Real},symm::Bool,d::Dict=Dict())
-      @assert dom[1] < dom[2] "Invalid support."
-      new(lowercase(name),w,Float64.(dom),symm,d)
+        @assert dom[1] < dom[2] "Invalid support."
+        new(lowercase(name),w,Float64.(dom),symm,d)
     end
 end
 
 struct MultiMeasure
-  name::Vector{String}
-  w::Function
-  w_uni::Vector{Function}
-  dom::Vector{Tuple{Float64,Float64}}
-  symmetric::Vector{Bool}
-  pars::Vector{Dict}
+    name::Vector{String}
+    w::Function
+    w_uni::Vector{Function}
+    dom::Vector{Tuple{Float64,Float64}}
+    symmetric::Vector{Bool}
+    pars::Vector{Dict}
 end
 
 # constructor for classic distributions
 function Measure(name::String,d::Dict=Dict())
     name = lowercase(name)
-    if name == "legendre"
-      s = (-1,1); symm = true
-      return Measure(name,w_legendre,s,symm,d)
-    elseif name == "jacobi"
-        s = (-1,1)
-        par1, par2 = d[:shape_a], d[:shape_b]
-        par1==par2 ? symm=true : symm=false
-        return Measure(name,build_w_jacobi(par1,par2),s,symm,d)
-    elseif name == "laguerre"
-        s = (0,Inf); symm = false
-        return Measure(name,w_laguerre,s,symm,d)
-    elseif name == "genlaguerre"
-        s = (0,Inf); symm = false
-        return Measure(name,build_w_genlaguerre(d[:shape]),s,symm,d)
-    elseif name == "hermite"
-        s = (-Inf,Inf); symm = true
-        return Measure(name,w_hermite,s,symm,d)
-    elseif name == "genhermite"
-        s = (-Inf,Inf); symm = true
-        return Measure(name,build_w_genhermite(Float64(d[:mu])),s,symm,d)
-    elseif name == "meixnerpollaczek"
-        s = (-Inf,Inf); symm = false
-        par1, par2 = d[:lambda], d[:phi]
-        return Measure(name,build_w_meixner_pollaczek(par1,par2),s,symm,d)
-  ########################################################
-  ########################################################
-  ########################################################
-  # measures corresponding to probability density functions:
-    elseif name == "gaussian"
-        s = (-Inf, Inf)
-        return Measure(name,w_gaussian,s,true,d)
-    elseif name == "uniform01"
-        s = (0,1)
-        return Measure(name,w_uniform01,s,true,d)
-    elseif name == "beta01"  # parameters of beta distribution
-        s = (0,1)
-        par1, par2 = d[:shape_a], d[:shape_b]
-        @assert par1 > 0 && par2 > 0 "Invalid shape parameters."
-        symm = (par1 == par2)
-    return Measure(name,build_w_beta(par1,par2),s,symm,d)
-        elseif name == "gamma"
-        shape, rate = d[:shape], d[:rate]
-        @assert rate==1. "rates different from one not yet supported."
-        s = (0,Inf)
-        return Measure(name,build_w_gamma(shape),s,false,d)
-    elseif name == "logistic"
-        s = (-Inf,Inf)
-        return Measure(name,w_logistic,s,true,d)
-    else
-        error("Measure named `$name` is not yet implemented.")
-    end
+    name == "legendre"         &&  return Measure(name,w_legendre,(-1,1),true,d)
+    name == "jacobi"           &&  d[:shape_a] > -1 && d[:shape_b] > -1 && return Measure(name,build_w_jacobi(d[:shape_a], d[:shape_b]),(-1,1),d[:shape_a] == d[:shape_b],d)
+    name == "laguerre"         &&  return Measure(name,w_laguerre,(0,Inf),false,d)
+    name == "genlaguerre"      &&  return Measure(name,build_w_genlaguerre(d[:shape]),(0,Inf),false,d)
+    name == "hermite"          &&  return Measure(name,w_hermite,(-Inf,Inf),true,d)
+    name == "genhermite"       &&  return Measure(name,build_w_genhermite(Float64(d[:mu])),(-Inf,Inf),true,d)
+    name == "meixnerpollaczek" &&  return Measure(name,build_w_meixner_pollaczek(d[:lambda], d[:phi]),(-Inf,Inf),false,d)
+    # measures corresponding to probability density functions:
+    name == "gaussian"  &&  return Measure(name,w_gaussian,(-Inf,Inf),true,d)
+    name == "uniform01" &&  return Measure(name,w_uniform01,(0,1),true,d)
+    name == "beta01"    &&  d[:shape_a] > 0 && d[:shape_b] > 0 &&  return Measure(name,build_w_beta(par1,par2),(0,1),d[:shape_a] == d[:shape_b],d)
+    name == "gamma"     &&  d[:rate] == 1. && return Measure(name,build_w_gamma(d[:shape]),(0, Inf),false,d)
+    name == "logistic"  &&  return Measure(name,w_logistic,(-Inf,Inf),true,d)
+    # error handling
+    name == "jacobi" && !(d[:shape_a] > -1 && d[:shape_b] > -1) &&  error("Invalid shape parameters.")
+    name == "beta01" && !(d[:shape_a] >  0 && d[:shape_b] >  0) &&  error("Invalid shape parameters.")
+    name == "gamma"  && !(d[:rate] == 1.) &&  error("Rates different from one not supported currently.")
+    error("A measure of name $name is not implemented.")
 end
 
 struct OrthoPoly
-  name::String
-  deg::Int64          # maximum degree
-  α::Vector{Float64}  # recurrence coefficients
-  β::Vector{Float64}  # recurrence coefficients
-  meas::Measure
-  # inner constructor
-  function OrthoPoly(name::String,deg::Int64,α::Vector{Float64},β::Vector{Float64},m::Measure)
-    @assert deg >= 0 "Degree has to be non-negative."
-    @assert length(α) == length(β) "Different number of recursion coefficients α and β supplied."
-    new(lowercase(name),deg,α,β,m)
-  end
+    name::String
+    deg::Int64          # maximum degree
+    α::Vector{Float64}  # recurrence coefficients
+    β::Vector{Float64}  # recurrence coefficients
+    meas::Measure
+    # inner constructor
+    function OrthoPoly(name::String,deg::Int64,α::Vector{Float64},β::Vector{Float64},m::Measure)
+        @assert deg >= 0 "Degree has to be non-negative."
+        @assert length(α) == length(β) "Different number of recursion coefficients α and β supplied."
+        new(lowercase(name),deg,α,β,m)
+    end
 end
 
 # constructor for classic distributions
 function OrthoPoly(name::String,deg::Int64,d::Dict=Dict();Nrec::Int64=deg+1)
   @assert Nrec >= deg + 1 "Not enough recurrence coefficients specified"
   name = lowercase(name)
-  if name == "legendre"
-    d = Dict()
-    a,b = rm_legendre(Nrec)
-  elseif name == "jacobi"
-    par1, par2 = d[:shape_a], d[:shape_b]
-    symm = (par1 == par2)
-    a,b = rm_jacobi(Nrec,Float64(par1),Float64(par2))
-  elseif name == "laguerre"
-    d = Dict()
-    a,b = rm_laguerre(Nrec)
-  elseif name == "genlaguerre"
-    p = d[:shape]
-    a,b = rm_laguerre(Nrec,Float64(p))
-  elseif name == "hermite"
-    d = Dict()
-    a,b = rm_hermite(Nrec)
-  elseif name == "genhermite"
-    p = d[:mu]
-    a,b = rm_hermite(Nrec,Float64(p))
-  elseif name == "meixnerpollaczek"
-    par1, par2 = d[:lambda], d[:phi]
-    a,b = rm_meixner_pollaczek(Nrec,Float64(par1),Float64(par2))
-  ########################################################
-  ########################################################
-  ########################################################
+  name == "legendre"         && return OrthoPoly(name,deg,rm_legendre(Nrec)...,Measure(name,Dict()))
+  name == "jacobi"           && return OrthoPoly(name,deg,rm_jacobi(Nrec,Float64(d[:shape_a]), Float64(d[:shape_b]))...,Measure(name,d))
+  name == "laguerre"         && return OrthoPoly(name,deg,rm_laguerre(Nrec)...,Measure(name,Dict()))
+  name == "genlaguerre"      && return OrthoPoly(name,deg,rm_laguerre(Nrec,Float64(d[:shape]))...,Measure(name,d))
+  name == "hermite"          && return OrthoPoly(name,deg,rm_hermite(Nrec)...,Measure(name,Dict()))
+  name == "genhermite"       && return OrthoPoly(name,deg,rm_hermite(Nrec,Float64(d[:mu]))...,Measure(name,d))
+  name == "meixnerpollaczek" && return OrthoPoly(name,deg,rm_meixner_pollaczek(Nrec,Float64(d[:lambda]),Float64(d[:phi]))...,Measure(name,d))
   # measures corresponding to probability density functions:
-  elseif name == "gaussian"
-    d = Dict()
-    a,b = r_scale(1/sqrt(2pi),rm_hermite_prob(Nrec)...)
-  elseif name == "uniform01"
-    d = Dict()
-    a,b = r_scale(1.,rm_legendre01(Nrec)...)
-  elseif name == "beta01"  # parameters of beta distribution
-    par1, par2 = d[:shape_a], d[:shape_b]
-    a,b = r_scale(1/beta(par1,par2),rm_jacobi01(Nrec,Float64(par2)-1,Float64(par1)-1)...)
-  elseif name == "gamma"
-    shape, rate = d[:shape], d[:rate]
-    @assert rate==1. "rates different from one not yet supported."
-    a,b = r_scale((rate^shape)/gamma(shape),rm_laguerre(Nrec,Float64(shape-1.))...)
-  elseif name == "logistic"
-    d = Dict()
-    a,b = r_scale(1.,rm_logistic(Nrec)...)
-  else
-    error("$name is not yet implemented.")
-  end
-  m = Measure(name,d)
-  return OrthoPoly(name,deg,a,b,m)
+  name == "gaussian"  && return OrthoPoly(name,deg,r_scale(1/sqrt(2pi),rm_hermite_prob(Nrec)...)...,Measure(name,Dict()))
+  name == "uniform01" && return OrthoPoly(name,deg,r_scale(1.,rm_legendre01(Nrec)...)...,Measure(name,Dict()))
+  name == "beta01"    && return OrthoPoly(name,deg,r_scale(1/beta(d[:shape_a],d[:shape_b]),rm_jacobi01(Nrec,Float64(d[:shape_b])-1,Float64(d[:shape_a])-1)...)...,Measure(name,d))
+  name == "gamma"     && return OrthoPoly(name,deg,r_scale((d[:rate]^d[:shape])/gamma(d[:shape]),rm_laguerre(Nrec,Float64(d[:shape]-1.))...)...,Measure(name,d))
+  name == "logistic"  && return OrthoPoly(name,deg,r_scale(1.,rm_logistic(Nrec)...)...,Measure(name,Dict()))
+  # error handling
+  error("$name is not yet implemented.")
 end
 
 # general constructor
@@ -159,7 +94,7 @@ function OrthoPoly(name::String,deg::Int64,m::Measure;Nrec=deg+1,Nquad=10*Nrec,q
 end
 
 function OrthoPoly(name::String,deg::Int64,w::Function,s::Tuple{Real,Real},symm::Bool,d::Dict=Dict();Nrec=deg+1,Nquad=10*Nrec,quadrature::Function=clenshaw_curtis,discretization::Function=stieltjes)
-  @assert Nrec>=deg+1 "Not enough recurrence coefficients specified"
+  @assert Nrec >= deg + 1 "Not enough recurrence coefficients specified"
   name = lowercase(name)
   m = Measure(name,w,s,symm,d)
   a,b = rm_compute(m,Nrec,Nquad,quadrature=quadrature,discretization=discretization)
@@ -167,16 +102,16 @@ function OrthoPoly(name::String,deg::Int64,w::Function,s::Tuple{Real,Real},symm:
 end
 
 struct Quad
-  name::String       # name of quadrature
-  Nquad::Int64          #   number of qudrature points
-  nodes::Vector{Float64}
-  weights::Vector{Float64}
-  meas::Measure
-  function Quad(name::String,N::Int64,nodes::Vector{Float64},weights::Vector{Float64},m::Measure)
-    @assert N >= 1 "Number of qudrature points has to be positive"
-    @assert length(nodes) == length(weights) "Inconsistent number of nodes and weights inconsistent."
-    new(lowercase(name),N,nodes,weights,m)
-  end
+    name::String              # name of quadrature
+    Nquad::Int64              # number of qudrature points
+    nodes::Vector{Float64}
+    weights::Vector{Float64}
+    meas::Measure
+    function Quad(name::String,N::Int64,nodes::Vector{Float64},weights::Vector{Float64},m::Measure)
+        @assert N >= 1 "Number of qudrature points has to be positive"
+        @assert length(nodes) == length(weights) "Inconsistent number of nodes and weights inconsistent."
+        new(lowercase(name),N,nodes,weights,m)
+    end
 end
 
 function Quad(N::Int64,m::Measure;quadrature::Function=clenshaw_curtis)
@@ -290,16 +225,3 @@ op::Union{OrthoPolyQ,MultiOrthoPoly}
     new(dim,T,g,opq)
   end
 end
-
-# struct PolyChaosExpansion
-#   name::String
-#   coeffs::Vector{Float64}
-#   dim::Int64
-#   op::Union{OrthoPoly,OrthoPolyQ,MultiOrthoPoly}
-#   function PolyChaosExpansion(n::String,c::Vector{Float64},d::Int64,op::Union{OrthoPoly,OrthoPolyQ,MultiOrthoPoly})
-#     @assert length(c)==d "length of coefficients inconsistent with dimension ($(length(c)) vs. $d)"
-#     new(n,c,d,op)
-#   end
-# end
-#
-# function PolyChaosExpansion(name)
