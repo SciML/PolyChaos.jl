90830d228f508b8abb2d68c8991c0616a2e3f258
timueh <tillmann.muehlpfordt@kit.edu>
2019-03-12 21:56:51 +0100

    modified tests for auxfuns.jl

--- test/auxfuns.jl
+++ test/auxfuns.jl
@@ -6,36 +6,36 @@ opq = OrthoPolyQ(op)
 mop = MultiOrthoPoly([op for i in 1:nunc],d)
 mopq = MultiOrthoPoly([opq for i in 1:nunc],d)
 @testset "dimenions" begin
-    @test isequal(dim(op), d + 1)
-    @test isequal(dim(opq), d + 1)
-    @test isequal(dim(mop), factorial(d + nunc) / (factorial(d) * factorial(nunc)))
+    @test isequal(PolyChaos.dim(op), d + 1)
+    @test isequal(PolyChaos.dim(opq), d + 1)
+    @test isequal(PolyChaos.dim(mop), factorial(d + nunc) / (factorial(d) * factorial(nunc)))
 end
 
 @testset "degrees" begin
-    @test isequal(deg(op),d)
-    @test isequal(deg(opq),d)
-    @test isequal(deg(mop),d)
+    @test isequal(PolyChaos.deg(op),d)
+    @test isequal(PolyChaos.deg(opq),d)
+    @test isequal(PolyChaos.deg(mop),d)
 end
 
 NW = [ opq.quad.nodes opq.quad.weights ]
 @testset "nodes and weights" begin
-    @test nw(opq) == NW
-    @test nw([opq for i in 1:nunc])[1] == [NW[:,1] for i in 1:nunc]
-    @test nw([opq for i in 1:nunc])[2] == [NW[:,2] for i in 1:nunc]
-    @test nw(mopq)[1] == [NW[:,1] for i in 1:nunc]
-    @test nw(mopq)[2] == [NW[:,2] for i in 1:nunc]
+    @test PolyChaos.nw(opq) == NW
+    @test PolyChaos.nw([opq for i in 1:nunc])[1] == [NW[:,1] for i in 1:nunc]
+    @test PolyChaos.nw([opq for i in 1:nunc])[2] == [NW[:,2] for i in 1:nunc]
+    @test PolyChaos.nw(mopq)[1] == [NW[:,1] for i in 1:nunc]
+    @test PolyChaos.nw(mopq)[2] == [NW[:,2] for i in 1:nunc]
 end
 
 c = [ op.α op.β ]
 @testset "coefficients" begin
-    @test coeffs(op) == c
-    @test coeffs(opq) == c
-    @test coeffs([op for i in 1:nunc])[1] == [ c[:,1] for i in 1:nunc ]
-    @test coeffs([op for i in 1:nunc])[2] == [ c[:,2] for i in 1:nunc ]
-    @test coeffs([opq for i in 1:nunc])[1] == [ c[:,1] for i in 1:nunc ]
-    @test coeffs([opq for i in 1:nunc])[2] == [ c[:,2] for i in 1:nunc ]
-    @test coeffs(mop)[1] == [ c[:,1] for i in 1:nunc ]
-    @test coeffs(mop)[2] == [ c[:,2] for i in 1:nunc ]
+    @test PolyChaos.coeffs(op) == c
+    @test PolyChaos.coeffs(opq) == c
+    @test PolyChaos.coeffs([op for i in 1:nunc])[1] == [ c[:,1] for i in 1:nunc ]
+    @test PolyChaos.coeffs([op for i in 1:nunc])[2] == [ c[:,2] for i in 1:nunc ]
+    @test PolyChaos.coeffs([opq for i in 1:nunc])[1] == [ c[:,1] for i in 1:nunc ]
+    @test PolyChaos.coeffs([opq for i in 1:nunc])[2] == [ c[:,2] for i in 1:nunc ]
+    @test PolyChaos.coeffs(mop)[1] == [ c[:,1] for i in 1:nunc ]
+    @test PolyChaos.coeffs(mop)[2] == [ c[:,2] for i in 1:nunc ]
 end
 
 @test issymmetric(op) == issymmetric(opq) == issymmetric(opq.quad)
