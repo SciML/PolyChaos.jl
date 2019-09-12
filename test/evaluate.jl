475f8446964231c0e25ee0ff9e1a9609f0394467
timueh <tillmann.muehlpfordt@kit.edu>
2019-09-12 16:34:45 +0200

    revised tests for evaluate.jl

--- test/evaluate.jl
+++ test/evaluate.jl
@@ -5,11 +5,23 @@ op = genLaguerreOrthoPoly(deg,1.23)
 
 n = 10
 X = [ exp(3 + rand()), exp.(3 .+ rand(n)) ]
+
 @testset "evaluate univariate basis at point(s)" begin
     for x in X
         @test evaluate(collect(0:deg),x,op.α,op.β) == evaluate(collect(0:deg),x,op)
         for d in 0:deg
-            @test evaluate(d,x,op.α,op.β) == evaluate(d,x,op)
+            if typeof(x) <: Real
+                @test evaluate(d,x,op.α,op.β) == evaluate(d,x,op) == evaluate(d,[x],op.α,op.β)
+            else
+                @test evaluate(d,x,op.α,op.β) == evaluate(d,x,op)
+            end
+        end
+
+        res = hcat(map(i->evaluate(i,x,op.α,op.β),collect(0:op.deg))...)
+        if typeof(x) <: Real
+            @test evaluate(x,op) == evaluate([x], op) == evaluate(collect(0:op.deg),[x],op) == evaluate(collect(0:op.deg),[x],op.α,op.β) == res
+        else
+            @test evaluate(x, op) == evaluate(collect(0:op.deg),x,op) == evaluate(collect(0:op.deg),x,op.α,op.β) == res
         end
     end
 end
@@ -29,6 +41,12 @@ X = [ exp.(3 .+ rand(nop)) , exp.(3 .+ rand(n,nop)) ]
 @testset "evaluate multivariate basis at point(s)" begin
     for x in X
         @test evaluate(mop.ind,x,coeffs(mop)...) == evaluate(mop.ind,x,mop) == evaluate(x,mop)
+        if typeof(x) <: Vector{<:Real}
+            @test evaluate(mop.ind,x,coeffs(mop)...) == evaluate(mop.ind,x,mop) == evaluate(x,mop) == evaluate(mop.ind,reshape(x,1,length(x)),coeffs(mop)...)
+            @test evaluate(x,mop) == evaluate(mop.ind,reshape(x,1,length(x)),mop) == evaluate(mop.ind,reshape(x,1,length(x)),coeffs(mop)...)
+        else
+            @test evaluate(x,mop) == evaluate(mop.ind,x,mop) == evaluate(mop.ind,x,coeffs(mop)...)
+        end
         for d in eachrow(mop.ind[1:3,:])
             @test evaluate(collect(d),x,coeffs(mop)...) == evaluate(collect(d),x,mop)
         end
