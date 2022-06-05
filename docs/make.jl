6d13031f57749a592a42a501c25c371a6e24d7b1
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-06-05 07:33:33 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,8 @@
 # Pkg.add("Documenter")
 using Documenter, PolyChaos
 
+include("pages.jl")
+
 makedocs(
     sitename = "PolyChaos.jl",
     format = Documenter.HTML(analytics = "UA-90474609-3",
@@ -10,26 +12,7 @@ makedocs(
     modules = [PolyChaos],
     authors = "tillmann.muehlpfordt@kit.edu",
     doctest = false,
-    pages = Any[
-        "index.md",
-        "type_hierarchy.md",
-        "Usage" => [
-            "numerical_integration.md",
-            "quadrature_rules.md",
-            "orthogonal_polynomials_canonical.md",
-            "gaussian_mixture_model.md",
-            "multiple_discretization.md",
-            "scalar_products.md",
-            "Polynomial Chaos" => [ "Basic Usage" => "pce_tutorial.md",
-                                    "Chi Squared, One DOF" => "chi_squared_k1.md",
-                                    "Chi Squared, Several DOFs" => "chi_squared_k_greater1.md",
-                                    "Random ODE" => "random_ode.md"
-                                  ],
-            "Optimal Power Flow" => "DCsOPF.md"
-            ],
-    "math.md",
-    "functions.md"
-    ]
+    pages = pages
 )
 
 
