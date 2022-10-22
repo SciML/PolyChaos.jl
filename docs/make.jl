2421263cdc739026ad417c30b94dcbc49cc1b78a
krishna bhogaonker <cyclotomiq@gmail.com>
2022-10-22 14:46:21 -0700

    updated README and make.jl as per sciml template

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,7 @@ include("pages.jl")
 makedocs(sitename = "PolyChaos.jl",
          format = Documenter.HTML(analytics = "UA-90474609-3",
                                   assets = ["assets/favicon.ico"],
-                                  canonical = "https://polychaos.sciml.ai/stable/"),
+                                  canonical = "https://docs.sciml.ai/PolyChaos/stable/"),
          modules = [PolyChaos],
          authors = "tillmann.muehlpfordt@kit.edu",
          doctest = false,
