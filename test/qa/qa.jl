67e44cb5860c14a7d551d6050b6ffc96aba1ca75
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-07-20 09:09:00 -0400

    Use default public API docs QA
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- test/qa/qa.jl
+++ test/qa/qa.jl
@@ -2,6 +2,5 @@ using SciMLTesting, PolyChaos, Test
 
 run_qa(
     PolyChaos; explicit_imports = true,
-    api_docs_kwargs = (; rendered = true),
     aqua_kwargs = (; ambiguities = (; recursive = false))
 )
